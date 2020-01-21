7c99cbd59530ae209537a5ff25afd5a0ccd0320c
Gary Sheppard <gsheppard@esri.com>
2020-01-21 17:57:16 -0500

    Calling async login function

--- Rust/wanderer/src/main.rs
+++ Rust/wanderer/src/main.rs
@@ -1,3 +1,5 @@
+use reqwest::Response;
+use reqwest::Result;
 use std::collections::HashMap;
 use std::io;
 
@@ -5,15 +7,11 @@ extern crate rpassword;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-async fn login(username: String, password: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn login(username: String, password: String) -> Result<Response> {
+    println!("This is login; username is {}", username);
     let client = reqwest::Client::new();
     // TODO do the actual login using client.post, instead of this demo
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-    Ok(())
+    reqwest::get("https://httpbin.org/ip").await
 }
 
 #[tokio::main]
@@ -27,6 +25,18 @@ async fn main() {
     let password = rpassword::read_password_from_tty(Some("Password: ")).unwrap();
     println!("Logging in as {}...", username);
     
-    let foo = login(username, password);
+    let foo = login(username, password).await;
+    match foo {
+        Ok(response) => {
+            println!("got a response");
+            match response.text().await {
+                Ok(text) => {
+                    println!("In JSON, it's this: {}", text);
+                },
+                Err(err) => println!("error here: {:?}", err),
+            }
+        },
+        Err(err) => println!("error parsing response: {:?}", err),
+    }
     println!("Looks like we made it!");
 }
