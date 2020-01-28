fd85773bc7e74912c80ea7828ea5b0d573dd37a4
Gary Sheppard <gsheppard@esri.com>
2020-01-28 14:20:59 -0500

    Getting city count and minimum population

--- Rust/wanderer/src/main.rs
+++ Rust/wanderer/src/main.rs
@@ -6,9 +6,11 @@ use std::collections::HashMap;
 use std::io;
 use uuid::Uuid;
 
+extern crate json;
 extern crate rpassword;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const FEATURE_LAYER_URL: &'static str = "https://services7.arcgis.com/iYTqAIgyDcVSpgzf/arcgis/rest/services/World_Cities/FeatureServer/0";
 
 #[derive(Deserialize)]
 struct TokenResponse {
@@ -68,8 +70,95 @@ async fn get_portal_self(client: &reqwest::Client, token: &String, referrer: &St
     }
 }
 
+async fn get_cities_count(client: &reqwest::Client, token: &String, referrer: &String) -> u32 {
+    let f_json = String::from("json");
+    let mut result: Result<Response> = client.get(format!("{}/query", FEATURE_LAYER_URL).as_str())
+        .query(&[
+            ("where", "population IS NOT NULL"),
+            ("returnCountOnly", "true"),
+            ("token", token),
+            ("referer", referrer),
+            ("f", &f_json),
+        ])
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            let response_result: Result<String> = response.text().await;
+            match response_result {
+                Ok(response_string) => {
+                    match json::parse(response_string.as_str())
+                        .unwrap()["count"].as_u32() {
+                        Some(count) => count,
+                        None => {
+                            println!("Count is null (this should never happen)");
+                            0
+                        },
+                    }
+                },
+                Err(err) => {
+                    println!("Couldn't parse response: {:?}", err);
+                    0
+                }
+            }
+        },
+        Err(err) => {
+            println!("Couldn't get portal self: {:?}", err);
+            0
+        },
+    }
+}
+
+async fn get_minimum_population(client: &reqwest::Client, token: &String, referrer: &String, city_count: u32) -> u32 {
+    let f_json = String::from("json");
+    let mut result: Result<Response> = client.get(format!("{}/query", FEATURE_LAYER_URL).as_str())
+        .query(&[
+            ("outFields", "population"),
+            ("where", "population IS NOT NULL"),
+            ("orderByFields", "population DESC"),
+            ("resultOffset", (city_count - 1).to_string().as_str()),
+            ("resultRecordCount", "1"),
+            ("returnGeometry", "false"),
+            ("token", token),
+            ("referer", referrer),
+            ("f", &f_json),
+        ])
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            let response_result: Result<String> = response.text().await;
+            match response_result {
+                Ok(response_string) => {
+                    match json::parse(response_string.as_str())
+                        .unwrap()["features"][0]["attributes"]["population"].as_u32() {
+                        Some(population) => population,
+                        None => {
+                            println!("Population is null (this should never happen)");
+                            0
+                        },
+                    }
+                },
+                Err(err) => {
+                    println!("Couldn't parse response: {:?}", err);
+                    0
+                }
+            }
+        },
+        Err(err) => {
+            println!("Couldn't get portal self: {:?}", err);
+            0
+        },
+    }
+}
+
 async fn play_game(client: &reqwest::Client, token: &String, referrer: &String, city_count: u32) {
-    println!("Let's play Wanderer with up to {} cities", city_count);
+    println!("Let's play Wanderer with {} cities", city_count);
+    
+    // Get the minimum population for cities in this game
+    let minimum_population = get_minimum_population(client, token, referrer, city_count).await;
+    println!("Minimum population: {}", minimum_population);
+    
     // We need the analysis URL
     let self_response: Result<PortalSelfResponse> = get_portal_self(client, token, referrer).await;
     match self_response {
@@ -113,7 +202,9 @@ async fn main() {
                         0 => 10,
                         1 => 100,
                         2 => 1000,
-                        3 => std::u32::MAX,
+                        3 => {
+                            get_cities_count(&reqwest_client, &json.token, &referrer).await
+                        },
                         _ => {
                             println!("Okay, then you get the default of 0 = easy.");
                             10
