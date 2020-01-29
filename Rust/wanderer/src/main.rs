abd280cc207e92747ae62e5eeec3dae58914d5bc
Gary Sheppard <gsheppard@esri.com>
2020-01-28 22:40:13 -0500

    Getting start and end cities; initial game loop

--- Rust/wanderer/src/main.rs
+++ Rust/wanderer/src/main.rs
@@ -1,16 +1,27 @@
+extern crate json;
+extern crate rand;
+extern crate rpassword;
+extern crate strfmt;
+
+use rand::Rng;
 use reqwest::RequestBuilder;
 use reqwest::Response;
 use reqwest::Result;
 use serde::Deserialize;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io;
+use strfmt::strfmt;
 use uuid::Uuid;
-
-extern crate json;
-extern crate rpassword;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const FEATURE_LAYER_URL: &'static str = "https://services7.arcgis.com/iYTqAIgyDcVSpgzf/arcgis/rest/services/World_Cities/FeatureServer/0";
+const WELCOME_MESSAGES: &[&str] = &[
+    "Though you've just arrived, you look around and immediately realize that you are in {city}.",
+    "Something in the air tells you you've just arrived in {city}.",
+    "That scent seems so familiar. \"Ah yes,\" you tell yourself. \"This could only be {city}.\".",
+    "The sunsets in {city} are so beautiful this time of year. If only you had time to linger.",
+];
 
 #[derive(Deserialize)]
 struct TokenResponse {
@@ -27,12 +38,37 @@ struct Service {
 #[derive(Deserialize)]
 struct HelperServices {
     analysis: Service,
+    geometry: Service,
 }
 
 #[derive(Deserialize)]
-struct PortalSelfResponse {
+struct PortalSelf {
     #[serde(rename = "helperServices")]
     helper_services: HelperServices,
+}
+
+#[derive(Deserialize)]
+struct City {
+    city: String,
+    lat: f32,
+    lng: f32,
+    country: String,
+    admin_name: String,
+    //#[serde(default)]
+    population: u32,
+    #[serde(rename = "FID")]
+    fid: u32,
+}
+
+#[derive(Deserialize)]
+struct CityFeature {
+    #[serde(rename = "attributes")]
+    city: City,
+}
+
+#[derive(Deserialize)]
+struct QueryResponse {
+    features: Vec<CityFeature>,
 }
 
 async fn login(client: &reqwest::Client, username: &String, password: &String, referrer: &String) -> Result<Response> {
@@ -49,7 +85,7 @@ async fn login(client: &reqwest::Client, username: &String, password: &String, r
         .await
 }
 
-async fn get_portal_self(client: &reqwest::Client, token: &String, referrer: &String) -> Result<PortalSelfResponse> {
+async fn get_portal_self(client: &reqwest::Client, token: &String, referrer: &String) -> Result<PortalSelf> {
     let f_json = String::from("json");
     let mut result: Result<Response> = client.get("https://www.arcgis.com/sharing/rest/portals/self")
         .query(&[
@@ -103,7 +139,7 @@ async fn get_cities_count(client: &reqwest::Client, token: &String, referrer: &S
             }
         },
         Err(err) => {
-            println!("Couldn't get portal self: {:?}", err);
+            println!("Couldn't get city count: {:?}", err);
             0
         },
     }
@@ -146,8 +182,170 @@ async fn get_minimum_population(client: &reqwest::Client, token: &String, referr
             }
         },
         Err(err) => {
-            println!("Couldn't get portal self: {:?}", err);
+            println!("Couldn't get minimum population: {:?}", err);
             0
+        },
+    }
+}
+
+async fn get_cities(client: &reqwest::Client, token: &String, referrer: &String, fids: Vec<u32>) -> Vec<CityFeature> {
+    let f_json = String::from("json");
+    let fid_strings: Vec<String> = fids.iter().map(ToString::to_string).collect();
+    let mut result: Result<Response> = client.get(format!("{}/query", FEATURE_LAYER_URL).as_str())
+        .query(&[
+            ("objectIds", &fid_strings.join(",")),
+            ("outFields", &String::from("*")),
+            ("token", token),
+            ("referer", referrer),
+            ("f", &f_json),
+        ])
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            //println!("Here it is:\n{}", &response.text().await.unwrap());
+            let response_result: Result<QueryResponse> = response.json().await;
+            match response_result {
+                Ok(query_response) => query_response.features,
+                Err(err) => {
+                    println!("Couldn't parse city query response: {:?}", err);
+                    Vec::new()
+                },
+            }
+        },
+        Err(err) => {
+            println!("Couldn't get query results: {:?}", err);
+            Vec::new()
+        },
+    }
+}
+
+async fn get_random_city_pair(client: &reqwest::Client, token: &String, referrer: &String, minimum_population: u32) -> Result<(City, City)> {
+    println!("Getting a random city pair with minimum population {}", minimum_population);
+    let f_json = String::from("json");
+    let mut result: Result<Response> = client.get(format!("{}/query", FEATURE_LAYER_URL).as_str())
+        .query(&[
+            ("outStatistics", r#"
+                [
+                    {
+                        "statisticType": "max",
+                        "onStatisticField": "FID",
+                        "outStatisticFieldName": "max_fid"
+                    },
+                    {
+                        "statisticType": "min",
+                        "onStatisticField": "FID",
+                        "outStatisticFieldName": "min_fid"
+                    }
+                ]
+            "#),
+            ("where", format!("population >= {}", minimum_population).as_str()),
+            ("token", token),
+            ("referer", referrer),
+            ("f", &f_json),
+        ])
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            let response_result: Result<String> = response.text().await;
+            match response_result {
+                Ok(response_string) => {
+                    let json_value = json::parse(response_string.as_str()).unwrap();
+                    let attributes = &json_value["features"][0]["attributes"];
+                    let min_fid = attributes["min_fid"].as_u32().unwrap();
+                    let max_fid = attributes["max_fid"].as_u32().unwrap();
+                    
+                    let mut cities: Vec<City> = Vec::new();
+                    let mut tried_fids = HashSet::new();
+                    while 2 > cities.len() {
+                        let mut rng = rand::thread_rng();
+                        let mut fids = Vec::new();
+                        while 2 > fids.len() {
+                            let fid = rng.gen_range(min_fid, max_fid + 1);
+                            if tried_fids.insert(fid) {
+                                fids.push(fid);
+                            }
+                        }
+                        let city_results = get_cities(client, token, referrer, fids).await;
+                        for city_feature in city_results {
+                            if 2 > cities.len() && city_feature.city.population >= minimum_population {
+                                cities.push(city_feature.city);
+                            }
+                        }
+                    };
+                    Ok((cities.remove(0), cities.remove(0)))
+                },
+                Err(err) => {
+                    println!("Couldn't parse statistics response: {:?}", err);
+                    Err(err)
+                }
+            }
+        },
+        Err(err) => {
+            println!("Couldn't get statistics: {:?}", err);
+            Err(err)
+        },
+    }
+}
+
+async fn get_distance(client: &reqwest::Client, token: &String, referrer: &String, portal_self: &PortalSelf, cities: &(&City, &City)) -> f32 {
+    println!("Geometry URL is {}", &portal_self.helper_services.geometry.url);
+    let f_json = String::from("json");
+    let mut result: Result<Response> = client.get(format!("{}/distance", &portal_self.helper_services.geometry.url).as_str())
+    // let mut result: Result<Response> = client.get(format!("{}/distance", FEATURE_LAYER_URL).as_str())
+        .query(&[
+            ("geometry1", r#"
+                {
+                  "geometryType" : "esriGeometryPoint",
+                  "geometry" : 
+                  {
+                    "x" : -118.15, "y" : 33.80
+                  }
+                }
+            "#),
+            ("geometry2", r#"
+                {
+                  "geometryType" : "esriGeometryPoint",
+                  "geometry" : 
+                  {
+                    "x" : -118.15, "y" : 33.90
+                  }
+                }
+            "#),
+            ("sr", "4326"),
+            ("distanceUnit", "9036"), // esriSRUnit_Kilometer
+            ("geodesic", "true"),
+            //("token", token),
+            //("referer", referrer),
+            ("f", &f_json),
+        ])
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            let response_result: Result<String> = response.text().await;
+            match response_result {
+                Ok(response_string) => {
+                println!("The response: {}", &response_string);
+                    match json::parse(response_string.as_str())
+                        .unwrap()["distance"].as_f32() {
+                        Some(distance) => distance,
+                        None => {
+                            println!("Distance is null (this should never happen)");
+                            std::f32::MAX
+                        },
+                    }
+                },
+                Err(err) => {
+                    println!("Couldn't parse response: {:?}", err);
+                    0.0
+                }
+            }
+        },
+        Err(err) => {
+            println!("Couldn't get distance: {:?}", err);
+            0.0
         },
     }
 }
@@ -155,17 +353,57 @@ async fn get_minimum_population(client: &reqwest::Client, token: &String, referr
 async fn play_game(client: &reqwest::Client, token: &String, referrer: &String, city_count: u32) {
     println!("Let's play Wanderer with {} cities", city_count);
     
+    // We need the portal self for its URLs
+    let portal_self = get_portal_self(client, token, referrer).await.unwrap();
+
     // Get the minimum population for cities in this game
     let minimum_population = get_minimum_population(client, token, referrer, city_count).await;
     println!("Minimum population: {}", minimum_population);
     
-    // We need the analysis URL
-    let self_response: Result<PortalSelfResponse> = get_portal_self(client, token, referrer).await;
-    match self_response {
-        Ok(portal_self) => {
-            println!("Analysis URL is {}", portal_self.helper_services.analysis.url);
+    // Get a couple of random cities
+    match get_random_city_pair(client, token, referrer, minimum_population).await {
+        Ok(cities) => {
+            println!("Hey, Wanderer! Let's see if you can make it to the secret destination.");
+            let mut current_city: &City = &cities.0;
+            let target_city: &City = &cities.1;
+            let mut distance_to_target: f32 = get_distance(client, token, referrer, &portal_self, &(current_city, target_city)).await;
+            let mut rng = rand::thread_rng();
+            let mut welcome_vars = HashMap::new();
+            
+            welcome_vars.insert(String::from("city"), &current_city.city);
+            println!("{}", strfmt(
+                WELCOME_MESSAGES[rng.gen_range(0, WELCOME_MESSAGES.len())],
+                &welcome_vars
+            ).unwrap());
+            
+            while true {
+                println!("You are now {:.0}km from your destination.", distance_to_target);
+                println!("What's next, Wanderer? (n, s, e, w, info)");
+                let mut cmd = String::new();
+                io::stdin().read_line(&mut cmd)
+                    .expect("Failed to read command");
+                cmd = String::from(cmd.trim()).to_lowercase();
+                let cmd = cmd.as_str();
+                match cmd {
+                    "n" | "s" | "e" | "w" => {
+                        println!("So you want to move {}", cmd);
+
+                        welcome_vars.insert(String::from("city"), &current_city.city);
+                        println!("{}", strfmt(
+                            WELCOME_MESSAGES[rng.gen_range(0, WELCOME_MESSAGES.len())],
+                            &welcome_vars
+                        ).unwrap());
+                    },
+                    "info" => {
+                        println!("So you want more info");
+                    },
+                    _ => {
+                        println!("I don't know how to {}", cmd);
+                    },
+                }
+            }
         },
-        Err(err) => println!("Error response for self: {:?}", err),
+        Err(err) => println!("No cities?! {}", err),
     }
 }
 
